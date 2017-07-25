5517c24a2f38c1345275baa3f58b7f32fde41e7c
hzchenweiyuan <hzchenweiyuan@corp.netease.com>
2017-07-25 12:08:00 +0800

    add style loader

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import _ from './lodash'
+import './style.css'
 function component(){
 	var element = document.createElement("div");
-	element.innerHTML = _.join(["hello",'webpack'],'!');
+	element.innerHTML = _.join(["hello",'webpack'],' ')
+	element.classList.add("hello");
 	return element
 }
 document.body.appendChild(component())
